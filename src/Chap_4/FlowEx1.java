3097aea53cced49eb0ffa7cb4352be0e9f745b00
DongWook Kim <dw_426@naver.com>
2024-07-03 20:04:24 +0900

    Chapter 4

--- src/Chap_4/FlowEx1.java
+++ src/Chap_4/FlowEx1.java
@@ -6,5 +6,7 @@ public class FlowEx1 {
         System.out.printf("x =%d일 때, 참인 것은 %n", x);
 
         if(x == 0) System.out.println("x==0");
+        if(x != 0) System.out.println("x != 0");
+
     }
 }
